50287bce86ae215887a6fc899ac4cd8ddb90516e
unknown <jose.cancino@tecsup.edu.pe>
2021-04-14 14:47:35 -0500

    Amplificando estadisticas

--- Laboratorio4/votaciones/App.js
+++ Laboratorio4/votaciones/App.js
@@ -3,24 +3,54 @@ import React, { useState} from 'react'
 import "./css/app.css";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  //save clicks of each button to its own state
+  const [bueno, setBueno] = useState(0)
+  const [normal, setNormal] = useState(0)
+  const [malo, setMalo] = useState(0)
+
+  
+
+  const total = bueno + malo + normal
+  const promedio = total / 3
+  const positivo = (bueno*100)/total
+
+  const clickbueno = () => {
+    console.log("estoy haciendo click")
+    setBueno(bueno +1)
+    
+  }
+
+  const clicknormal = () => {
+    console.log("estoy haciendo click") 
+    setNormal(normal +1)   
+   
+
+  }
+
+  const clickmalo = () => {
+    console.log("estoy haciendo click")
+    setMalo(malo +1)  
+    
+
+  }
+
 
   return (
     <div className="container">
       <h1 className="titulo" >TU OPINION IMPORTA</h1>
       <div className ="secbotones">
-        <button className ="boton" > BUENO</button>
-        <button className ="boton"  >MEDIO </button>
-        <button className ="boton" >MALO </button>
+        <button className ="boton" onClick={clickbueno}> BUENO </button>
+        <button className ="boton" onClick={clicknormal} >NORMAL </button>
+        <button className ="boton" onClick={clickmalo}>MALO </button>
       </div>
       <div className="estadistica">
         <h2 className="textestadistica">Estadisticas</h2>
-        <p className="textvoto">Votaciones de Bueno</p>
-        <p className="textvoto">Votaciones de Medio</p>
-        <p className="textvoto">Votaciones de Malo</p>
+        <p className="textvoto">Votaciones de Bueno: {bueno}</p>
+        <p className="textvoto">Votaciones de Normal: {normal}</p>
+        <p className="textvoto">Votaciones de Malo: {malo}</p>
+        <p className="textvoto">Total de Votos: {total}</p>
+        <p className="textvoto">Promedio de Votos: {promedio}</p>
+        <p className="textvoto">Votos Positivos: {positivo}%</p>
       </div>
     </div>
   )
